178b51a7df29b141c85cc8050499d60d9899db15
mblarrabee <mblarrabee421@gmail.com>
2015-07-11 12:28:25 -0600

    finished closures practis, added new way to only run ANY function one time

--- practice.js
+++ practice.js
@@ -9,11 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+console.log(inner());
 
 
 
@@ -32,8 +32,8 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
-
+var callJake = callFriend();
+console.log(callJake("435-215-9248"));
 
 
 //Next Problem
@@ -44,12 +44,19 @@ var callFriend = function(){
   Write a function called makeCounter that makes the following code work properly.
 */
 
+  function makeCounter(){
+    var counter = 0;
+    return function(){
+      return ++counter;
+    };
+  };
+
   //Code Here
   var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+  console.log(count()); // 1
+  console.log(count()); // 2
+  console.log(count()); // 3
+  console.log(count()); // 4
 
 
 
@@ -62,8 +69,59 @@ var callFriend = function(){
   (which invokes the original function that was passed in) that can only ever be executed once.
   Once completed, add a second argument that allows the function to be invoked N number of times.
   After the function has been called N number of times, console.log('STAHHP');
-*/
 
+
+
+*/
+  //This works, but only for the function explicitly defined by runIt
+function simpleFunc(){
+  console.log("The function executed");
+};
+
+function runNTimes(passedFunc, N){
+  return function(){
+    for(var i = N; i > 0; i--){
+        passedFunc();
+    };
+    console.log("The function has already been executed " + N + " times.");
+  };
+};
+
+var runIt = runNTimes(simpleFunc, 33);
+
+runIt(simpleFunc);
+
+
+
+
+function simpleFunc(){
+  console.log("The function executed");
+};
+
+function simpleFunc2(){
+  console.log("The 2nd function executed");
+};
+
+function simpleFunc3(){
+  console.log("The 3rd function executed");
+};
+
+function simpleFunc4(){
+  console.log("The 4th function executed");
+};
+
+
+//This works for ANY function passed into it and will only allow it to run one time.
+
+function runOnce(passedFunc){
+  if(!passedFunc.alreadyran){
+    passedFunc();
+    passedFunc.alreadyran = true;
+  }
+  else{
+    console.log("The function has already been executed");
+  };
+};
 
 
 
